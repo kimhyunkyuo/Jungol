8028191ba84b55e12e6e91301a7303b56afcb732
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-26 14:49:50 +0900

    9003번 문제입니다.

---
+++ Jungol/src/q9003/Main.java
@@ -0,0 +1,12 @@
+package q9003;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("세 번째 프로그램입니다. \n줄을 바꾸어 출력합니다 \n여기까지 출력하고 \n줄을 바꿉니다.");
+		
+	}
+	
+
+}
