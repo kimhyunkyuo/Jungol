399dfb9396580102e53c929ebce8a824d1d54463
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-26 17:30:24 +0900

    511번 문제입니다.

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,14 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x = 45;
+			int b = 9;
+		
+		System.out.println("55 - 10 ="+x );
+		System.out.println("2008 - 1999 ="+b);
+	}
+
+}
