f6a70f2f8c6bd190bc08ebe6d383513a8f28e5ff
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 14:08:00 +0900

    512번 문제

---
+++ Jungol/src/q512/Main.java
@@ -0,0 +1,20 @@
+package q512;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 49;
+		double y = 0.2683;
+		
+		
+	System.out.print(x); 
+	System.out.print(" * 0.268300 = "); 
+	System.out.print(  x * 0.2683+"00");	
+		
+//	%d는 소수말고 숫자	System.out.printf("%d * %f = %f\n", x, y, x * y);
+	
+
+	}
+
+}
