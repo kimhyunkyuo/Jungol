648c93d586580b2d9564b760168bbb3594a6f408
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 18:02:40 +0900

    108번문제

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,14 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nub1 = 50;
+		double d1 = 100.12;
+		
+//		System.out.printf("%.2f * %d = 5006",d1,nub1,d1*nub1 );
+		System.out.printf("%.2f * %d = %.0f",d1,nub1,d1*nub1 );
+	}
+
+}
