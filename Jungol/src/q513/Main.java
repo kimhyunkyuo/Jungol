c19c88b89630bbd698b8d14759fa8e2fc7e59c5c
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 17:13:28 +0900

    513번 문제

---
+++ Jungol/src/q513/Main.java
@@ -0,0 +1,16 @@
+package q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		double yd = 91.44;
+		double in = 2.54;
+		
+		System.out.printf("%4.1fyd = %2.1fcm\n", 2.1, 2.1*yd);
+		System.out.printf("%4.1fin = %5.1fcm\n", 10.5, 10.5*in);
+		
+	}
+
+}
