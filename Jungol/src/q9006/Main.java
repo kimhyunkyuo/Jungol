92f705f81ea24d0cb8d0289f4fe05f77f62a2151
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 11:51:28 +0900

    9006번 문제입니다.

---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,15 @@
+package q9006;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 123;
+		 double b = 6.500000;
+		
+		 System.out.println("수를 출력할 때는 다옴표를 생략합니다.");
+		System.out.println(a);
+		System.out.println(String.format("%.6f", b));
+	}
+
+}
