6f48f5d6b37a7994d9f828d377ef70105e1a2f56
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 17:39:44 +0900

    107번 문제

---
+++ Jungol/src/q107/Main.java
@@ -0,0 +1,15 @@
+package q107;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double nub1 = 80.50;
+		double nub2 = 22.34;
+		
+	System.out.printf("%10.2f %9.2f %9.2f",nub1,nub2,(nub1+nub2));	
+		
+	}
+
+}
