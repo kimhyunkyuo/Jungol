b054aa2ec28c149ae189b352e33afd0be799622b
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 10:34:05 +0900

    508번 문제

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,19 @@
+package q508;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.out.println("      item"+"     count" +"     price");
+//		System.out.println("       pen"+"        20" +"       100");
+//		System.out.println("      note"+"         5" +"        95");
+//		System.out.println("    eraser"+"       110" +"        97");
+		
+		
+		String[] a = {"item","count","price"};
+		
+		System.out.printf("%10s%10s%10s\n",a[0],a[1],a[2]);
+	}
+
+}
