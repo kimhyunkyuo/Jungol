3af14abe9b6fdc6883c2f70e2eee11c5e457ca67
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 18:02:54 +0900

    for while dowhile문

---
+++ Jungol/src/q536/Main.java
@@ -0,0 +1,25 @@
+package q536;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		int i = 1;
+//		while (i<=15) {
+//			System.out.print(i+" ");
+//			 i++;
+			 
+			 
+		//for 문
+			for (int e = 1; e<=15; e++) { 
+			System.out.print(e + " ");	
+			
+			 
+		//do while문	
+			int k = 1;
+			
+			do{System.out.print(k++ +" ");
+			 }while(k<16);
+	}
+
+}
