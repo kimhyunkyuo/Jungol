6691a24e82454ab43b768c4f2068951a0d0c4575
Class01 <Class01@DESKTOP-P2NCJP3>
2023-05-01 10:02:16 +0900

    514 프린트

---
+++ Jungol/src/q514/Main.java
@@ -0,0 +1,18 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("height = ");
+		int weight = sc.nextInt();
+		sc.close();
+		
+		System.out.println("Your height is "+ weight + "cm.");
+	}
+
+}
